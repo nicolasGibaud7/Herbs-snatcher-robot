0454b627afb11f6688785151abaccc9f4f53ed4c
nicolas_gibaud <n.gibaud@catie.fr>
2020-03-18 16:21:12 +0100

    Send pwm commands when green shape is detected

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 from centerVideo import * 
+from sensor import *
 
 if __name__ == "__main__":
-    r = get_target_position(True, True, 56)
-    print(f'Final result : {r}')
+    while true:
+        x, y = get_target_position(True, True, 56)
+        pwm_orders = {}
+        if x < 640 / 2 and y < 480 / 2:
+            pwm_orders[1] = "300"
+            pwm_orders[2] = "300"
+            pwm_orders[3] = "000"
+            pwm_orders[4] = "000"
+        elif x > 640 / 2 and y < 480 / 2:
+            pwm_orders[1] = "000"
+            pwm_orders[2] = "300"
+            pwm_orders[3] = "000"
+            pwm_orders[4] = "300"
+        elif x < 640 / 2 and y > 480 / 2:
+            pwm_orders[1] = "300"
+            pwm_orders[2] = "000"
+            pwm_orders[3] = "300"
+            pwm_orders[4] = "000"
+        elif x > 640 / 2 and y > 480 / 2:
+            pwm_orders[1] = "000"
+            pwm_orders[2] = "000"
+            pwm_orders[3] = "300"
+            pwm_orders[4] = "300"
+
+        with Serial(
+            port = "/dev/ttyACM0", baudrate=115200, timeout=1, writeTimeout=1
+        ) as port_serie:
+            for num_pwm, angle in pwm_orders:
+                print(f"PWM {num_pwm} : {angle} degrés")
+                message = bytearray([str(num_pwm), ';', angle[0], angle[1], angle[2], 'a'])
+                port_serie.write(message)
+                time.sleep(0.001)
