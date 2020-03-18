f53820d3a75d9d00aef7b88aa6e99dd12346a52b
nicolas_gibaud <n.gibaud@catie.fr>
2020-03-18 16:28:27 +0100

    Fix some syntax errors

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from centerVideo import * 
+from centerVideo import *
 from sensor import *
 
 if __name__ == "__main__":
@@ -24,6 +24,7 @@ if __name__ == "__main__":
             pwm_orders[1] = "000"
             pwm_orders[2] = "000"
             pwm_orders[3] = "300"
+<<<<<<< Updated upstream
             pwm_orders[4] = "300"
 
         with Serial(
@@ -32,5 +33,16 @@ if __name__ == "__main__":
             for num_pwm, angle in pwm_orders:
                 print(f"PWM {num_pwm} : {angle} degrés")
                 message = bytearray([str(num_pwm), ';', angle[0], angle[1], angle[2], 'a'])
+=======
+            pwm_orders[4] = "300"
+
+        with Serial(
+            port="/dev/ttyACM0", baudrate=115200, timeout=1, writeTimeout=1
+        ) as port_serie:
+            for num_pwm, angle in pwm_orders:
+                message = bytearray(
+                    [str(num_pwm), ";", angle[0], angle[1], angle[2], "a"]
+                )
+>>>>>>> Stashed changes
                 port_serie.write(message)
                 time.sleep(0.001)
