6f7985cdace0f60f6207ef72762d26153bc38b26
nicolasGibaud7 <nicolas.gibaud7@gmail.com>
2020-03-18 12:25:42 +0100

    Add stm adaptater for pwm commands

---
+++ adaptateur_stm/MDK-ARM/RTE/_Robot_YDays/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Robot_YDays' 
+ * Target:  'Robot_YDays' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
